0218019be87e7c48ea71ded33021153e3bfe8eb6
ASUS <zijing.yang@my.jcu.edu.au>
2025-06-15 18:23:08 +0800

    Two tasks: 1.Added initial subject_reader.py and subject_data.txt files. 2.Modified load_data to return a nested list with student numbers as integers. Task1. Add initial subject_reader.py and subject_data.txt for reading and processing subject data. -Add initial: Indicates the first addition of files. -subject_reader.py and subject_data.txt: Specifies the files added. -for reading and processing subject data: Describes the purpose of the files.
    Task2. Refactor load_data to return nested list with integer student numbers and add display_subject_details for formatted subject output.
    -Refactor load_data: Notes the modification of the load_data function.
    -to return nested list with integer student numbers: Specifies the change to return a nested list and convert student numbers.
    -add display_subject_details: Indicates the new function.
    -for formatted subject output: Explains the function’s purpose.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,11 @@
+"""
+CP1404 practical 4
+
+"""
+
+numbers = [3,1,4,1,5,9,2]
+
+numbers[0] = "ten"         # Change first element to string "ten"
+numbers[-1] = 1            # Change last element to 1
+print(numbers[2:])         # Print all elements except first two
+print(9 in numbers)        # Print whether 9 is in numbers
